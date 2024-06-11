6898b2b79507b2708c9bab41d036e8ada8e9ce92
ahk42 <ahk42@pitt.edu>
2024-06-11 16:26:58 -0400

    update new peak plots

--- ATAC-Seq/deeptools.sh
+++ ATAC-Seq/deeptools.sh
@@ -62,41 +62,81 @@ GENE_ANNOTATION="/bgfs/ialdiri/Genomes/mm39.ncbiRefSeq.gtf.gz"
 #     -T "Sox2 Knockout" \
 #     --averageTypeSummaryPlot mean
 
-# # Change reference to center
-# computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
-#     -S "${BIGWIG_FILES[@]}" \
-#     -R /bgfs/ialdiri/ATAC-Seq/Sox2_ATAC-Seq/outDir/bowtie2/merged_library/macs2/narrow_peak/consensus_summits.bed \
-#     --binSize $WINDOW_SIZE \
-#     -o matrix.gz \
-#     --outFileSortedRegions sorted_regions.bed \
-#     --sortRegions descend \
-#     --sortUsing mean \
-#     --missingDataAsZero \
-#     --verbose -p max --skipZeros --smartLabels
-
-# plotHeatmap -m matrix.gz -out Sox2_ATAC-Seq_heatmap.png \
-#     --colorMap Blues  \
-#     --refPointLabel "Center" --verbose \
-#     -T "Sox2 Knockout" \
-#     --averageTypeSummaryPlot mean 
-
-# Differential Chromatin Accessibility
-
-PEAKS_DIR="/bgfs/ialdiri/Sox2_Vsx2/Sox2_Vsx2_Peaks"
-
+# Change reference to center
 computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
     -S "${BIGWIG_FILES[@]}" \
-    -R "$PEAKS_DIR/sox2_vsx2_shared_peaks.bed" "$PEAKS_DIR/sox2_unique_peaks.bed" "$PEAKS_DIR/vsx2_unique_peaks.bed" \
+    -R /bgfs/ialdiri/ATAC-Seq/Sox2_ATAC-Seq/outDir/bowtie2/merged_library/macs2/narrow_peak/Sox2_ATAC_consensus_peaks.bed \
     --binSize $WINDOW_SIZE \
-    -o diff_matrix.gz \
+    -o matrix.gz \
     --outFileSortedRegions sorted_regions.bed \
     --sortRegions descend \
     --sortUsing mean \
     --missingDataAsZero \
     --verbose -p max --skipZeros --smartLabels
 
-plotHeatmap -m diff_matrix.gz -out diff_heatmap.png \
+plotHeatmap -m matrix.gz -out Sox2_ATAC_consensus_peak_heatmap.png \
     --colorMap Blues  \
     --refPointLabel "Center" --verbose \
-    -T "Differential Chromatin Accessibility" \
+    -T "Sox2 ATAC KO with consensus peaks" \
     --averageTypeSummaryPlot mean 
+
+# Differential Chromatin Accessibility
+
+PEAKS_DIR="/bgfs/ialdiri/Sox2_Vsx2/Sox2_Vsx2_Peaks"
+
+# computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
+#     -S "${BIGWIG_FILES[@]}" \
+#     -R "$PEAKS_DIR/sox2_vsx2_shared_peaks.bed" "$PEAKS_DIR/sox2_unique_peaks.bed" "$PEAKS_DIR/vsx2_unique_peaks.bed" \
+#     --binSize $WINDOW_SIZE \
+#     -o diff_matrix.gz \
+#     --outFileSortedRegions sorted_regions.bed \
+#     --sortRegions descend \
+#     --sortUsing mean \
+#     --missingDataAsZero \
+#     --verbose -p max --skipZeros --smartLabels
+
+# plotHeatmap -m diff_matrix.gz -out diff_heatmap.png \
+#     --colorMap Blues  \
+#     --refPointLabel "Center" --verbose \
+#     -T "Differential Chromatin Accessibility" \
+#     --averageTypeSummaryPlot mean 
+
+# Create Tornado Plots for ATAC w/ Sox2 and Non Sox2 Bound regions
+# Create files from bedtools intersect -v
+WT_BIGWIG_FILES=("$BIGWIG_DIR/WT1_REP1.bigWig" "$BIGWIG_DIR/WT2_REP1.bigWig")
+ATAC_PEAKS_DIR=(/bgfs/ialdiri/ATAC-Seq/Sox2_ATAC-Seq/outDir/bowtie2/merged_library/macs2/narrow_peak)
+
+computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
+    -S "${WT_BIGWIG_FILES[@]}" \
+    -R "$ATAC_PEAKS_DIR/sox2_vsx2_shared_peaks.bed" \
+    --binSize $WINDOW_SIZE \
+    -o bound_matrix.gz \
+    --outFileSortedRegions sorted_regions.bed \
+    --sortRegions descend \
+    --sortUsing mean \
+    --missingDataAsZero \
+    --verbose -p max --skipZeros --smartLabels
+
+plotHeatmap -m bound_matrix.gz -out Sox2_ATAC_WT_Bound_heatmap.png \
+    --colorMap Blues  \
+    --refPointLabel "Center" --verbose \
+    -T "Sox2 ATAC WT Bound by Sox2" \
+    --averageTypeSummaryPlot mean 
+
+# Unbound regions Sox2
+computeMatrix reference-point --referencePoint center -b 2000 -a 2000 \
+    -S "${WT_BIGWIG_FILES[@]}" \
+    -R "$ATAC_PEAKS_DIR/sox2_vsx2_shared_peaks.bed" \
+    --binSize $WINDOW_SIZE \
+    -o unbound_matrix.gz \
+    --outFileSortedRegions sorted_regions.bed \
+    --sortRegions descend \
+    --sortUsing mean \
+    --missingDataAsZero \
+    --verbose -p max --skipZeros --smartLabels
+
+plotHeatmap -m unbound_matrix.gz -out Sox2_ATAC_WT_Unbound_heatmap.png \
+    --colorMap Blues  \
+    --refPointLabel "Center" --verbose \
+    -T "Sox2 ATAC WT Unbound by Sox2" \
+    --averageTypeSummaryPlot mean
---
+++ CR_Chip-Seq/Plots/README.md
@@ -0,0 +1,5 @@
+# SOX2 CUT&RUN and Vsx2 ChIP-Seq Plots
+
+## All of these plots were created using the new peak files that were generated with 'bedtools intersect'
+
+### 
